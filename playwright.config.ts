0cbb4741e6dad0b0c31134d8ba38aa727ac12cd5
oleg.pimkin <oleg.pimkin@steercrm.com>
2025-08-16 17:06:07 +0300

    fixes issues in comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://practicesoftwaretesting.com',
+    testIdAttribute: 'data-test',
+  
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,13 +2,13 @@ import { test, expect } from '@playwright/test';
 test('my first test', async ({ page }) => {
   
   test.skip(!!process.env.CI, 'Test file is skipped in CI due to Cloudflare protection.');
-  await page.goto('https://practicesoftwaretesting.com/auth/login');
-  await page.locator('id=email').fill('customer@practicesoftwaretesting.com');
-  await page.locator('id=password').fill('welcome01');
-  await page.locator('data-test=login-submit').click();
-  await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
-  const titleText = await page.locator('data-test=page-title').innerText();
+  await page.goto('/auth/login');
+  await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
+  await page.getByTestId('password').fill('welcome01');
+  await page.getByTestId('login-submit').click();
+  await expect(page).toHaveURL(/\/account/);
+  const titleText = await page.getByTestId('page-title').innerText();
   expect(titleText).toBe('My account');
-  const userMenuText = await page.locator('data-test=nav-menu').innerText();
+  const userMenuText = await page.getByTestId('nav-menu').innerText();
   expect(userMenuText).toContain('Jane Doe');
 });
