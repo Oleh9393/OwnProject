e2f4539634f1ffd945be5ef2c4dd42e622af6953
oleg.pimkin <oleg.pimkin@steercrm.com>
2025-12-07 19:44:47 +0200

    fix auth

--- tests/auth.setup.spec.ts
+++ tests/auth.setup.spec.ts
@@ -1,32 +1,14 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
 import { usersCreds } from '../data/creds';
 
-const authFile = 'auth/session.json';
+test('login as a customer', async ({ page }) => {
+    //test.skip(!!process.env.CI, 'Test file is skipped in CI due to Cloudflare protection.');
 
-test('login via API', async ({ request, page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/auth/login');
+    await loginPage.performLogin(usersCreds.customer.email, usersCreds.customer.password);
 
-    const response = await request.post('https://api.practicesoftwaretesting.com/users/login', {
-        data: {
-            email: usersCreds.customer.email,
-            password: usersCreds.customer.password,
-        },
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
-
-    expect(response.status()).toBe(200);
-
-  
-    const loginResponse = await response.json() as { access_token: string };
-    const token = loginResponse.access_token;
-
-    await page.goto('/');
-    await page.evaluate((accessToken) => {
-        localStorage.setItem('access_token', accessToken);
-    }, token);
-
-
-    await page.context().storageState({ path: authFile });
-});
+    await expect(page).toHaveURL(/\/account/);
+    await page.context().storageState({ path: 'auth/session.json' });
+})
